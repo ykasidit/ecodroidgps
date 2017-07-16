b28d5dee57336594fa71a3ff40a5aacbb34864ea
Kasidit Yusuf <ykasidit@gmail.com>
2017-07-16 11:30:16 +0700

    use nc broadcast TODO

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
@@ -7,11 +8,12 @@
 #include <sys/mman.h>
 #include <libgen.h>
 
-enum {MAX_SIZE_USB_GPS_READ_BUFF = 2048, max_fn_buf_len = 1024};
+enum {MAX_SIZE_USB_GPS_READ_BUFF = 512, max_fn_buf_len = 1024};
 
 struct g_shared_data {
 	char g_usb_gps_read_line[MAX_SIZE_USB_GPS_READ_BUFF];
 	char g_usb_gps_read_line_gpgll[MAX_SIZE_USB_GPS_READ_BUFF]; //reader proc can use this if some N previous of its access to g_usb_gps_read_line didn't match $GPGLL cases...
+	uint64_t g_usb_gps_read_line_id; //dont write g_usb_gps_read_line if the id hasn't changed
 	char exec_dir[max_fn_buf_len];
 	pthread_mutex_t mutex;
 };
@@ -20,16 +22,16 @@ struct g_shared_data* g_shared = NULL;
 
 int print_nmea = 0;
 
-int usb_gps_reader_run()
+int usb_gps_reader_run(char* gps_dev_path)
 {
-	FILE* f;
-	char* gps_dev_path = "/dev/ttyACM0";
-	char* ret_fgets;
+	FILE* f;	
+	int rret;
 
 	char usb_gps_read_line[MAX_SIZE_USB_GPS_READ_BUFF];
 	int usb_gps_read_line_len = 0;
 
 	f = fopen(gps_dev_path,"r");
+	
 	if (!f) {
 		printf("FATAL: failed to open gps_dev_path: %s\n", gps_dev_path);
 		return -1;
@@ -37,8 +39,13 @@ int usb_gps_reader_run()
 
 	while (1) {
 		usb_gps_read_line[0] = 0;
-		ret_fgets = fgets(usb_gps_read_line, MAX_SIZE_USB_GPS_READ_BUFF, f);
+		
+		rret = fread(usb_gps_read_line, 1, MAX_SIZE_USB_GPS_READ_BUFF-1, f);
+		printf("reader: usb_gps_read_line_len: %d\n", rret);
+
+		usb_gps_read_line[MAX_SIZE_USB_GPS_READ_BUFF-1] = 0;		
 		usb_gps_read_line_len = strlen(usb_gps_read_line);
+
 
 		if (usb_gps_read_line_len) {
 			if (usb_gps_read_line_len > 1) {
@@ -49,6 +56,7 @@ int usb_gps_reader_run()
 
 				pthread_mutex_lock(&g_shared->mutex);
 				strcpy(g_shared->g_usb_gps_read_line, usb_gps_read_line);
+				g_shared->g_usb_gps_read_line_id++;
 				pthread_mutex_unlock(&g_shared->mutex);
 
 				if (strstr(usb_gps_read_line, "$GPGLL")) {
@@ -76,8 +84,11 @@ int bt_server_run(int instance)
 	char buf[buf_max_len] = { 0 };
 	int wret;
 	char usb_gps_read_line[MAX_SIZE_USB_GPS_READ_BUFF];
-	FILE* p;
+	uint64_t usb_gps_read_line_id;
+	uint64_t written_usb_gps_read_line_id = 0;
+	FILE* subp;
 	char bzc_rfcomm_cmd[buf_max_len] = { 0 };
+	int microsecs_to_wait_if_id_hasnt_changed = 500*1000; //500ms
 
 	// Open bluez-compassion rfcomm profile server
 	
@@ -88,9 +99,14 @@ int bt_server_run(int instance)
 
 	sprintf(bzc_rfcomm_cmd, "%s/bluez-compassion/rfcomm -p '/ecodroidgps_port_%d' -n 'spp' -s -C %d -u '0x1101'", g_shared->exec_dir, instance, instance);
 	printf("bzc_rfcomm_cmd: %s\n", bzc_rfcomm_cmd);
-	exit(0);
-	p = popen("", "w");
+	subp = popen(bzc_rfcomm_cmd, "w");
 
+	if (subp == NULL) {
+		printf("FATAL: bt_server_run: pid %d, instance %d popen returned null - ABORT\n", getpid(), instance);
+	}
+
+	int fd = fileno(subp);	
+		
 	// Once connected, we can write to its tx pipe (or its stdin).
 
 	// Make a reader to its rx pipe (or its stdout) too to avoid broken or blocked pipe cases.
@@ -99,31 +115,65 @@ int bt_server_run(int instance)
 	while (1) {
 
 		pthread_mutex_lock(&g_shared->mutex);
+		usb_gps_read_line_id = g_shared->g_usb_gps_read_line_id;
+		pthread_mutex_unlock(&g_shared->mutex);
+		if (written_usb_gps_read_line_id == usb_gps_read_line_id) {
+			if (print_nmea)
+				printf("instance %d: usb_gps_read_line_id hasn't changed - wait...\n", instance);
+			usleep(microsecs_to_wait_if_id_hasnt_changed);
+			continue;
+		}
+
+		written_usb_gps_read_line_id = usb_gps_read_line_id;		
+
+		pthread_mutex_lock(&g_shared->mutex);
 		strcpy(usb_gps_read_line, g_shared->g_usb_gps_read_line);
 		pthread_mutex_unlock(&g_shared->mutex);
-
+			
 		int len = strlen(usb_gps_read_line);
 		if (len) {
-		  //TODO write to stdin of bt server proc - wret = write(client, usb_gps_read_line, len);
-		  printf("instance %d: wret %d\n", instance, wret);
+		  //write to stdin of bt server proc
+			printf("bt_server_run: calling fputs on p\n");			
+			wret = write(fd, usb_gps_read_line, len);
+			printf("bt_server_run: wret %d\n", wret);
+			if (print_nmea) {
+				printf("bt_server_run: instance %d: pre write usb_gps_read_line: %s\n", instance, usb_gps_read_line);
+			}
+			printf("bt_server_run: instance %d: wret %d\n", instance, wret);
+			if (wret == 0) {
+				printf("bt_server_run: WARNING: 0 bytes written - instance %d: wret %d\n", instance, wret);
+			}
 		} else {
-			printf("instance %d: found read_line len 0 - sleep 1 sec to retry\n", instance);
+			printf("bt_server_run: instance %d: found read_line len 0 - sleep 1 sec to retry\n", instance);
 			sleep(1);
 		}
 	}
 
-	//TODO: cleanup/close the proc
+	printf("bt_server_run: instance %d closing subp\n", instance);
+	
+	pclose(subp);
+	subp = NULL;
+
+	printf("bt_server_run: instance %d exit\n", instance);
 
 	return 0;
 }
 
 int main(int argc, char **argv)
 {
-	enum{N_MAX_SERVERS = 7};
+	enum{N_MAX_SERVERS = 1};
 	enum{N_MAX_SUBPROCS = N_MAX_SERVERS+1};
 	int pids[N_MAX_SUBPROCS];
 	long usb_gps_read_ts = 0;
 	int i;
+
+	if (argc < 2) {
+		if (argc == 1)
+			printf("usage: %s <usb_gps_char_dev>\n", argv[0]);
+		else
+			printf("usage: this_binary <usb_gps_char_dev>\n");
+		exit(1);
+	}
 
 	char exec_path[max_fn_buf_len] = { 0 };
 	int rret = readlink("/proc/self/exe", exec_path, max_fn_buf_len);
@@ -175,10 +225,24 @@ int main(int argc, char **argv)
 
 			if (i == 0) {
 				//read from usb gps into buffer
-				usb_gps_reader_run();
+				int ret;
+				while (1) {
+					printf("usb_gps_reader_run: starting...\n");
+					ret = usb_gps_reader_run(argv[1]);
+					printf("WARNING: usb_gps_reader_run exit with code %d - restart it after 3 secs\n", ret);
+					sleep(3);
+				}
+				
 			} else {
 				//take the read usb gps buffer and write to connected bt clients...
-				bt_server_run(i);
+				int ret;
+				while (1) {
+					printf("bt_server_run: instance %d starting...\n", i);
+					ret = bt_server_run(i);
+					printf("WARNING: bt_server_run instance %d exit with code %d - restart it after 3 secs\n", i, ret);
+					sleep(3);
+				}
+				
 			}
 			break;
 		} else {
@@ -195,7 +259,7 @@ int main(int argc, char **argv)
 		pthread_mutex_lock(&g_shared->mutex);
 		strcpy(usb_gps_read_line, g_shared->g_usb_gps_read_line);
 		pthread_mutex_unlock(&g_shared->mutex);
-		if (print_nmea) {
+		if (1) {
 			printf("father_proc: g_usb_gps_read_line: %s\n", usb_gps_read_line);
 		}
 	};
