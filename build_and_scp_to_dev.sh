cb7a982015ab0c32084a8003b511e867063bc41f
Kasidit Yusuf <ykasidit@gmail.com>
2017-09-01 16:10:49 +0700

    build and push to dev via scp and extract via ssh working

--- build_and_scp_to_dev.sh
+++ build_and_scp_to_dev.sh
@@ -15,7 +15,7 @@ rm -f edg_release*.tar.gz
 DATE=$(date +%d%m%y)
 DATED_TAR_NAME="edg_release_${DATE}.tar.gz"
 
-tar -czf $DATED_TAR_NAME *.so ecodroidgps.py bt_spp_profile.py
+tar -czf $DATED_TAR_NAME *.so ecodroidgps.py bt_spp_profile.py edg_0.lic
 
 echo created $DATED_TAR_NAME done...
 
@@ -25,3 +25,7 @@ echo "scp to dev: $1 password $SSH_PASS"
 scp "$DATED_TAR_NAME" $SSH_USER@$1:~
 exit_if_failed
 echo "success - scp ret: $?"
+
+ssh $SSH_USER@$1 "rm -rf edg ; mkdir edg && cd edg && tar -xzf ../$DATED_TAR_NAME"
+exit_if_failed
+echo "success - ssh extract ret: $?"
---
+++ clean.sh
@@ -0,0 +1,1 @@
+rm *.so ; rm *.c; rm *.o
