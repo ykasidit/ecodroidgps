0f16b82bed4714d7c3c6fd3b570fbc1542febb48
Kasidit Yusuf <ykasidit@gmail.com>
2017-11-26 09:40:29 +0700

    add pi addr

--- gen_edg_lic.py
+++ gen_edg_lic.py
@@ -8,7 +8,7 @@ target_lic_fp = "/data/edg.lic"
 print "generating license file: {}".format(target_lic_fp)
 with open(sys.argv[1], "r") as rf:
     addrs = rf.readlines()
-    with open(target_lic_fp, "w") as wf:
+    with open(target_lic_fp, "wb") as wf:
         for mac_addr_colon_bdaddr in addrs:
             mac_addr_colon_bdaddr = mac_addr_colon_bdaddr.strip()
             for i in range(3):
@@ -20,7 +20,7 @@ with open(sys.argv[1], "r") as rf:
             print "mac_addr:", mac_addr_colon_bdaddr
             shaer = hashlib.sha1()
             shaer.update("edg")
-            shaer.update(mac_addr_colon_bdaddr+":edg_kub")
+            shaer.update(mac_addr_colon_bdaddr.lower()+":edg_kub")
             shaer.update("edg")
             this_sha = shaer.hexdigest()
             wf.write(this_sha+"\n")
