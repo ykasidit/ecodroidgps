a6b03c87b60ebdd05e4646c871189e4d180d84a5
Kasidit Yusuf <ykasidit@gmail.com>
2017-11-26 09:20:48 +0700

    lic file adj

--- gen_edg_lic.py
+++ gen_edg_lic.py
@@ -11,10 +11,11 @@ with open(sys.argv[1], "r") as rf:
     with open(target_lic_fp, "w") as wf:
         for mac_addr_colon_bdaddr in addrs:
             mac_addr_colon_bdaddr = mac_addr_colon_bdaddr.strip()
-            shaer = hashlib.sha1()
-            shaer.update(str(uuid.uuid4()))
-            random_sha = shaer.hexdigest()
-            wf.write(random_sha+"\n")
+            for i in range(3):
+                shaer = hashlib.sha1()
+                shaer.update(str(uuid.uuid4()))
+                random_sha = shaer.hexdigest()
+                wf.write(random_sha+"\n")
 
             print "mac_addr:", mac_addr_colon_bdaddr
             shaer = hashlib.sha1()
