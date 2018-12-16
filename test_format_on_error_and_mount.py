090a09f359ec91074ae071ecf6c942b931c439e1
Kasidit Yusuf <ykasidit@gmail.com>
2018-12-16 19:15:49 +0700

    add format_on_error_and_mount py

--- test_format_on_error_and_mount.py
+++ test_format_on_error_and_mount.py
@@ -2,11 +2,23 @@ import os
 
 
 def test():
-    cmd = "python format_on_error_and_mount.py --dev_to_dir_list /dev/mmcblk0p2:/data,/dev/mmcblk0p3:/config"
-    print "test cmd:", cmd
-    ret = os.system(cmd)
-    print "cmd ret:", ret
-    assert ret == 0
+
+    run_cmd = "python format_on_error_and_mount.py --dev_to_dir_list /dev/mmcblk0p2:/data,/dev/mmcblk0p3:/config"
+
+    cmds = [
+        run_cmd, # normal run
+        "umount /data && umount /config",  # unmount before try corrupt device...        
+        "dd if=/dev/zero bs=1 count=10 of=/dev/mmcplk0p2 seek=10000",  # try corrupt device
+        "dd if=/dev/zero bs=1 count=10 of=/dev/mmcplk0p3 seek=10000",  # try corrupt device        
+        run_cmd,
+        run_cmd
+    ]
+
+    for cmd in cmds:
+        print "test cmd:", cmd
+        ret = os.system(cmd)
+        print "cmd ret:", ret
+        assert ret == 0
 
     
 if __name__ == "__main__":
